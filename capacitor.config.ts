15b2ca03cb92ce1bdbbd15aa99f8d94c1ff7a1f2
rpinheir0 <140416454+rpinheir0@users.noreply.github.com>
2026-05-26 20:11:52 -0300

    save II

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.futquina.app',
+  appName: 'FutQuina',
+  webDir: 'dist'
+};
+
+export default config;
