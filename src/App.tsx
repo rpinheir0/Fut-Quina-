f92167a0ebe484acc0cc2462fda1c8842e57d2c3
rpinheir0 <140416454+rpinheir0@users.noreply.github.com>
2026-05-30 12:50:32 -0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -14468,7 +14468,7 @@ function GroupApp({
                   mainRef.current.scrollTo({ top: 0, behavior: "smooth" });
                 }
               }}
-              className="fixed bottom-28 right-5 z-[110] w-14 h-14 bg-emerald-500 text-white rounded-full shadow-[0_8px_30px_rgb(0,0,0,0.12)] border border-black/5 flex items-center justify-center cursor-pointer transition-colors hover:bg-emerald-600 hover:shadow-[0_8px_30px_rgb(0,0,0,0.16)]"
+              className="fixed bottom-36 right-5 z-[110] w-14 h-14 bg-emerald-500 text-white rounded-full shadow-[0_8px_30px_rgb(0,0,0,0.12)] border border-black/5 flex items-center justify-center cursor-pointer transition-colors hover:bg-emerald-600 hover:shadow-[0_8px_30px_rgb(0,0,0,0.16)]"
             >
               <motion.div
                 animate={{ y: [0, -4, 0] }}
